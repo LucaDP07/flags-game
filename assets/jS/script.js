cb916486bc40cf5db38bb3ebc8d5c7cd2b9b8ba9
LucaDP07 <luke07@hotmail.it>
2022-05-27 23:50:32 +0000

    Add style to show and hide the flags on the game board

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -1,11 +1,40 @@
 /* --------- Start Button */
 
 function startGame() {
-gameLoop();
+    gameLoop();
 }
+
+/* --------- Game Loop */
+
+var loops = 0;
+var peopleVisible = false;
 
 function gameLoop() {
-    alert("Game over!");
-    setTimeout (gameLoop, 3000);
+    peopleVisible = !peopleVisible;
+    createCharacters();
+    flashCharacters();
+    loops++;
+    if (loops < 12) {
+        setTimeout(gameLoop, 3000);
+    } else {
+        alert("Game over!");
+    }
+
 }
 
+function createCharacters() {
+    var board = document.getElementById("board");
+    for(var index = 0; index < 6; index ++) {
+        board.children[index].innerHTML = "Guest";
+    }
+    var randomNumber = Math.floor(Math.random() * 6) + 1;
+    board.children[randomNumber-1].innerHTML ="Thief";
+}
+
+function flashCharacters() {
+    var board = document.getElementById("board");
+    var classToSet = peopleVisible ? "flag visible" : "flag hidden";
+    for (var index = 0; index < 6; index++) {
+        board.children[index].className = classToSet;
+    }
+}
