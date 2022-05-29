b61268fbd5ff6c113f574704650b47d4d2497f51
LucaDP07 <luke07@hotmail.it>
2022-05-29 00:11:41 +0000

    Add images for the flags and a background

--- assets/jS/script.js
+++ assets/jS/script.js
@@ -13,18 +13,14 @@ var gameScore = o;
 
 /* --------- Game Loop */
 
-var loops = 0;
-var peopleVisible = false;
-
 function gameLoop() {
     peopleVisible = !peopleVisible;
     createCharacters();
-    flashCharacters();
     loops++;
     if (loops < 12) {
-        setTimeout(gameLoop, 3000);
+        setTimeout(gameLoop, peopleVisible ? 1000 : 3000);
     } else {
-        alert("You scored" + gameScore);
+        alert("You scored " + gameScore);
     }
 
 }
@@ -36,14 +32,16 @@ function createCharacters() {
     var classToSet = peopleVisible ? "flag visible" : "flag hidden";
     for(var index = 0; index < 6; index ++) {
         board.children[index].className = classToSet;
-        board.children[index].innerHTML = "Correct Flag";
+        board.children[index].innerHTML = "";
         board.children[index].onclick = function() {
             gameScore += -2;
         }
     }
     var randomNumber = Math.floor(Math.random() * 6) + 1;
-    board.children[randomNumber-1].innerHTML ="Wrong Flag";
+    board.children[randomNumber-1].innerHTML ="";
     board.children[randomNumber-1].onclick = function() {
         gameScore++;
     }
+    board.children[randomNumber-1].className = classToSet + " wrong-flag";
 }
+
